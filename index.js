8241c493839ebc08054c8e4e7e6930bb7d2371f2
Wilfredo Eduardo Villegas Méndez <eduardo78villegas@estudiantec.cr>
2025-05-14 01:00:47 -0600

    Prueba 2

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const port =3000;
 app.get('/',(req,res)=>{
-	res.send("hello world desde Deployment");
+	res.send("hello world desde Deployment V2");
 })
 
 app.listen(port,(error)=>{
