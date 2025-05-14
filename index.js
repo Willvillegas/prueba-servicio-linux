3cd73d8f5287160a9e2580b773c9971d7e4b5801
Wilfredo Eduardo Villegas Méndez <eduardo78villegas@estudiantec.cr>
2025-05-14 13:34:34 -0600

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const port =3000;
 app.get('/',(req,res)=>{
-	res.send("hello world desde Deployment V2");
+	res.send("hello world desde Deployment V2 V3");
 })
 
 app.listen(port,(error)=>{
